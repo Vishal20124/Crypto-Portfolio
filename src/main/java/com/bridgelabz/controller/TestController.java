c1a2681cf0d27ccf2327ca5271ede1b93b4e206f
Vishal20124 <vishalpeela@Vishals-MacBook-Pro-5.local>
2025-05-27 14:46:23 +0530

    first vishal changes

--- src/main/java/com/bridgelabz/controller/TestController.java
+++ src/main/java/com/bridgelabz/controller/TestController.java
@@ -1,12 +1,5 @@
 package com.bridgelabz.controller;
 
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/test")
 public class TestController {
-    @GetMapping
-    public String hello() {
-        return "Crypto Portfolio App is running!";
-    }
+
 }
