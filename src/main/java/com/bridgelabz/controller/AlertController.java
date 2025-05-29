85e7f3ecd43ede3d6995c7c6afb1fa0345b3bb0e
rahulr2004 <rahultanikonda@gmail.com>
2025-05-29 11:12:15 +0530

    Alerts are done

--- src/main/java/com/bridgelabz/controller/AlertController.java
+++ src/main/java/com/bridgelabz/controller/AlertController.java
@@ -1,5 +1,32 @@
 package com.bridgelabz.controller;
 
+import com.bridgelabz.entity.Alert;
+import com.bridgelabz.service.AlertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alerts")
 public class AlertController {
 
+    @Autowired
+    private AlertService alertService;
+
+    @PostMapping("/create")
+    public Alert createAlert(@RequestBody Alert alert) {
+        return alertService.createAlert(alert);
+    }
+
+    @GetMapping("/my")
+    public List<Alert> getMyAlerts(@RequestParam Long userId) {
+        return alertService.getAlertsByUserId(userId);
+    }
+
+    @GetMapping("/triggered")
+    public List<Alert> getTriggeredAlerts() {
+        return alertService.getTriggeredAlerts();
+    }
 }
+
